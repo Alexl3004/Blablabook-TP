88cbbf5286d470a7b7fd53e5d56c2ba045d8544c
Alexandre Loncle <129631073+Alexl3004@users.noreply.github.com>
2026-03-07 14:44:08 +0100

    ajout card dans l'accueil

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -40,6 +40,27 @@
                 </p>
               </div>
             </section>
+
+            <section class="features">
+              <div class="feature-card">
+                <h3>Catalogue Immense</h3>
+                <p>Accédez à des milliers de références pour tous les goûts.</p>
+              </div>
+              <div class="feature-card">
+                <h3>Ma Collection</h3>
+                <p>
+                  Suivez vos livres en cours, gérez vos piles à lire, et classez
+                  vos lectures en pause, lues ou abandonnées.
+                </p>
+              </div>
+              <div class="feature-card">
+                <h3>Suggestions</h3>
+                <p>
+                  Laissez-vous surprendre par une sélection renouvelée à chaque
+                  visite.
+                </p>
+              </div>
+            </section>
           {/if}
 
           <section class="suggestions">
@@ -115,12 +136,15 @@
     width: 100%;
     display: flex;
     flex-direction: column;
-    align-items: center;
   }
 
   .section-title {
     display: flex;
-    justify-content: center;
+    justify-content: start;
+    margin-left: 1rem;
+  }
+  .suggestions :global(.carousel-container) {
+    margin-top: 0;
   }
 
   h2 {
@@ -131,7 +155,38 @@
     box-shadow: var(--shadow);
     border: 1px solid rgba(233, 228, 219, 0.1);
   }
+  .features {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+    gap: 1.5rem;
+    max-width: 1000px;
+    margin:1rem;
+  }
 
+  .feature-card {
+    background: var(--color-white);
+    padding: 1.5rem;
+    border-radius: 20px;
+    box-shadow: var(--shadow);
+    text-align: center;
+    border: 1px solid var(--color-border);
+    transition: transform 0.3s ease;
+  }
+
+  .feature-card h3 {
+    font-size: 1.1rem;
+    margin-bottom: 0.5rem;
+    color: var(--color-secondary);
+  }
+
+  .feature-card p {
+    font-size: 0.9rem;
+    opacity: 0.8;
+    line-height: 1.4;
+  }
+  .is-connected {
+    margin-top: 1.5rem;
+  }
   @media (max-width: 900px) {
     .welcome {
       padding: 2rem 1.5rem;
@@ -141,6 +196,12 @@
     }
     h2 {
       font-size: 1.5rem !important;
+    }
+    .feature-card h3 {
+      font-size: 1rem;
+    }
+    .feature-card p {
+      font-size: 0.8rem;
     }
   }
 
@@ -160,9 +221,20 @@
       font-size: 1.3rem !important;
       padding: 0.6rem 1.5rem;
     }
-  }
 
-  .is-connected {
-    margin-top: 1.5rem;
+    .features {
+      grid-template-columns: repeat(2, 1fr);
+      max-width: 600px;
+      margin: 1rem;
+    }
+    .feature-card {
+      padding: 0.5rem;
+    }
+    .feature-card h3 {
+      font-size: 1rem;
+    }
+    .feature-card p {
+      font-size: 0.8rem;
+    }
   }
 </style>
